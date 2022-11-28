8d8e903140c8493154523e051ab1278948eb625b
AdrianPerFer <chopon007@gmail.com>
2022-11-28 09:01:31 +0100

    Ejercicios added

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -1,17 +1,17 @@
 /* 
- * Escribe un programa que pase de binario a decimal.
+ * Escribe un programa que pase de decimal a binario.
  * 
  * @author: Adrián Perogil Fernández
  */
 import matematicas.Funciones;
 import java.util.Scanner;
-public class Ejercicio17 {
+public class Ejercicio18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número binario: ");
-        String num = sc.nextLine();
-        int decimalConvertido = Integer.parseInt(num, 2);
-        System.out.println(decimalConvertido);
+        int num = sc.nextInt();
+        String binario = Integer.toBinaryString(num);
+        System.out.println(binario);
         sc.close();
     }
 }
